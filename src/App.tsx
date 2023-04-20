58e5becae299930db8550bf04abfd98a625fbb27
iblurdesigner <nevomen@gmail.com>
2023-04-20 17:28:21 -0500

    first commit / node version 18.15.0

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+import { TODO_FILTERS } from './consts'
+import { type TodoTitle, type FilterValue, type TodoId, type Todo as TodoType } from './types'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Ver el Twitch de midu',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Aprender React con TypeScript',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Sacar ticket de la miduFest',
+    completed: false
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
+
+  const handleRemove = ({ id }: TodoId): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = (
+    { id, completed }: Pick<TodoType, 'id' | 'completed'>
+  ): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter)
+  }
+
+  const handleRemoveAllCompleted = (): void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
+  const activeCount = todos.filter(todo => !todo.completed).length
+  const completedCount = todos.length - activeCount
+
+  const filteredTodos = todos.filter(todo => {
+    if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+    if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+    return todo
+  }
+  )
+
+  const handleTodo = ({ title }: TodoTitle): void => {
+    const newTodo = {
+      title,
+      id: crypto.randomUUID(),
+      completed: false
+    }
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className='todoapp'>
+      <Header onAddTodo={handleTodo}/>
+      <Todos
+        onToggleCompleteTodo = {handleCompleted}
+        onRemoveTodo = {handleRemove}
+        todos={filteredTodos}
+      />
+      <Footer
+        activeCount={activeCount}
+        completedCount={completedCount}
+        filterSelected={filterSelected}
+        handleFilterChange={handleFilterChange}
+        onClearCompleted={handleRemoveAllCompleted}
+        />
+    </div>
+  )
+}
+
+export default App
